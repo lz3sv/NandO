372bffed2268f10d4753361cd292241783d569f1
lz3sv <voichev@yahoo.com>
2024-08-14 23:18:41 +0300

    midReadMe

--- client/src/components/Not/Not404.jsx
+++ client/src/components/Not/Not404.jsx
@@ -6,7 +6,7 @@ export default function Not404(){
         <section id="not-found-page">
         <div className="not-found-page-container">
             <h1>404</h1>
-            <h2>Page Not Found</h2>
+            <h2>Страницата не е намерена</h2>
             <p>Страницата, която търсите не съществува, или е възникнал проблем. Идете към <Link to="/catalog"
                     >Последни публикации</Link>.
             </p>
